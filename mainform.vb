2a50052df5438ee8b172a8b29a1aa92f93771972
hoho <hoho915571@gmail.com>
2023-07-05 23:42:24 -0700

    delete engine

--- mainform.vb
+++ mainform.vb
@@ -1,43 +1,45 @@
-﻿Imports System.Diagnostics
+﻿Imports System
+Imports System.IO
 Imports System.Net
+Imports System.Text
 
 Public Class mainform
+
     Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
         rtbAnswer.Text = "Thinking..."
-        ' Create a request to the website URL
-        Dim request As HttpWebRequest = DirectCast(
-            WebRequest.Create($"http://hoho9155.pythonanywhere.com/ask/{txtQuestion.Text.Trim()}"),
-            HttpWebRequest
-        )
-
-        ' Get the response from the website
         Try
-            Using response As HttpWebResponse = DirectCast(request.GetResponse(), HttpWebResponse)
-                ' Check if the response is successful
-                If response.StatusCode = HttpStatusCode.OK Then
-                    ' Get the response stream
-                    Using stream As System.IO.Stream = response.GetResponseStream()
-                        ' Read the content from the stream
-                        Dim reader As New System.IO.StreamReader(stream)
-                        Dim content As String = reader.ReadToEnd()
+            ' Replace "YOUR_URL_HERE" with the actual URL you want to stream from
+            Dim url As String = $"http://hoho9155.pythonanywhere.com/ask/{txtQuestion.Text.Trim()}"
 
-                        ' Display the content in a text box or perform further processing
-                        rtbAnswer.Text = content
-                    End Using
-                Else
-                    ' Handle unsuccessful response code here
-                    rtbAnswer.Text = $"Error occurred. Status code: {response.StatusCode}"
+            Dim request As WebRequest = WebRequest.Create(url)
+            Dim response As WebResponse = request.GetResponse()
+
+            Dim stream As Stream = response.GetResponseStream()
+            Dim reader As New StreamReader(stream)
+
+            rtbAnswer.Text = ""
+
+            While True
+                Dim line As String = reader.ReadLine()
+
+                If line Is Nothing Then
+                    Exit While
                 End If
-            End Using
-        Catch ex As Exception
-            rtbAnswer.Text = ex.Message
-        End Try
 
+                rtbAnswer.AppendText(line & Environment.NewLine)
+                Application.DoEvents()
+            End While
+        Catch ex As Exception
+            MessageBox.Show("An error occurred: " & ex.Message)
+        End Try
     End Sub
+
 
     Private Sub txtQuestion_KeyUp(sender As Object, e As KeyEventArgs) Handles txtQuestion.KeyUp
         If e.KeyCode = Keys.Enter Then
             ' btnSend_Click(sender, e)
         End If
     End Sub
+
+
 End Class
